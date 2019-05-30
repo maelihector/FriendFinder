3fef6c8c7270448fbaa683f9ecae99e32acab64c
mhector <maelihector@gmail.com>
2019-05-30 16:44:14 -0500

    Add two more dummy friends to have matches for first few survey submis.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,53 @@
-var friendsArray = [{
-  name: "Ryan Gosling",
-  photo: "https://www.huffingtonpost.com/2013/01/15/ryan-gosling-knits_n_2482136.html",
-  scores: [
-    "2",
-    "3",
-    "3",
-    "1",
-    "1",
-    "4",
-    "1",
-    "2",
-    "5",
-    "4"
-  ]
-}];
+var friendsArray = [
+  {
+    name: "Ryan Gosling",
+    photo: "https://www.huffingtonpost.com/2013/01/15/ryan-gosling-knits_n_2482136.html",
+    scores: [
+      "2",
+      "3",
+      "3",
+      "1",
+      "1",
+      "5",
+      "4",
+      "5",
+      "5",
+      "4"
+    ]
+  },
+  {
+    name: "Kate McKinnon",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Kate_McKinnon_2018_%28cropped%29.jpg/220px-Kate_McKinnon_2018_%28cropped%29.jpg",
+    scores: [
+      "2",
+      "1",
+      "1",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "2",
+      "1"
+    ]
+  },
+  {
+    name: "Barack Obama",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/8/8d/President_Barack_Obama.jpg",
+    scores: [
+      "3",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "3",
+      "5",
+      "4"
+    ]
+  }
+];
 
 // Export the array. 
 module.exports = friendsArray;
